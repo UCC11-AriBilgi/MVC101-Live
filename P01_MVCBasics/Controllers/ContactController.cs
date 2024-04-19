ef74e59836880e500ef87a0a0022beaf12459fb4
ukaracivio <umit.karacivi@outlook.com>
2024-04-19 21:40:46 +0300

    Add project files.

---
+++ P01_MVCBasics/Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace P01_MVCBasics.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ P01_MVCBasics/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using P01_MVCBasics.Models;
+using System.Diagnostics;
+
+namespace P01_MVCBasics.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index() 
+        {
+            return View(); // Index isimli (Index.cshtml) view a git ..goster
+        }
+
+        public IActionResult Index2() // Sadece Index2 isimli metotoda git...
+        {
+            return Content("Burası view ı olmayan bir metotdur....");
+        }
+
+        public IActionResult Privacy() //Privacy isimli(Privacy.cshtml) view a git..goster
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
