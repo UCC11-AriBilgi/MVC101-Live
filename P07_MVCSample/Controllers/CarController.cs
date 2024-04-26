431b2eaf8865e747a0b6040a622c1c645e2f4fb8
ukaracivio <umit.karacivi@outlook.com>
2024-04-26 20:31:51 +0300

    Some changes

--- P07_MVCSample/Controllers/CarController.cs
+++ P07_MVCSample/Controllers/CarController.cs
@@ -34,19 +34,32 @@ namespace P07_MVCSample.Controllers
             return RedirectToAction("Index"); // İşlem gerçekleştikten sonra direkt olarak yine Index View a gönderiyorum.
         }
 
-        public IActionResult Details()
+        public IActionResult Details(int id)
         {
-            // Listeleme yapacak
+            // Index view üzerinde basıldığında ilgili kaydın detay bilgilerini bir form yapısı şeklinde gösterecek
+            var car= CarData.Cars.Where(c=> c.CarId==id).FirstOrDefault(); // gelen id parametresine göre ilgili id yi listede ara bulduğunda view a gönder.
 
-            return View(); // CarData içersindeki varolan bilgiyi Index View üzerine gönder
+            return View(car); // CarData içersindeki varolan bilgiyi Index View üzerine gönder
         }
 
+        //GET
+        public IActionResult Edit(int id)
+        {
+            // View(Index,Details) tarafından gönderilen id bilgisine göre ilgili kayıt değişiklik yapılabilecek şekilde Edit view a gönderecek
+            var car = CarData.Cars.Where(c => c.CarId == id).FirstOrDefault();
+
+            return View(car); // CarData içersindeki varolan bilgiyi Edit View üzerine gönder
+        }
+
+        //POST
+        [HttpPost]
         public IActionResult Edit()
         {
             // Listeleme yapacak
 
             return View(); // CarData içersindeki varolan bilgiyi Index View üzerine gönder
         }
+
 
         public IActionResult Delete()
         {
