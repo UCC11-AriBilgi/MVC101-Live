49148f45afe2a096cea4b420fc21e90c8df231f8
ukaracivio <umit.karacivi@outlook.com>
2024-04-26 19:47:28 +0300

    Some changes

--- P07_MVCSample/Controllers/CarController.cs
+++ P07_MVCSample/Controllers/CarController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using P07_MVCSample.Data;
+using P07_MVCSample.Models;
 
 namespace P07_MVCSample.Controllers
 {
@@ -12,11 +13,25 @@ namespace P07_MVCSample.Controllers
             return View(CarData.Cars); // CarData içersindeki varolan bilgiyi Index View üzerine gönder
         }
 
+        // GET
+        [HttpGet]
         public IActionResult Create()
         {
-            // Yeni bir veri gireceğim Create View a gidecek
+            // Yeni bir veri girebileceğim Create View a gidecek
 
             return View(); // 
+        }
+
+        // POST
+        // View tarafından submit edildiğinde gelinecek/dönülecek olan Action
+        [HttpPost]
+        public IActionResult Create(Car newcar)
+        {
+            // Create View dan gelecek olan ilgili modelin verilerini listeme ekleyeceğim.
+
+            CarData.Cars.Add(newcar);
+
+            return RedirectToAction("Index"); // İşlem gerçekleştikten sonra direkt olarak yine Index View a gönderiyorum.
         }
 
         public IActionResult Details()
